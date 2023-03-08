68db7a03c89fd4425e29789e2482fe5268bef449
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-08 17:15:21 +0530

    feat: add redis store and get in auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const AuthServices = require('./../services/auth.service');
+const { storeToken, getToken } = require('./../utils/Redis/index');
 
 const registerUser = async (req, res) => {
   const { username, password } = req.body;
@@ -14,6 +15,7 @@ const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
     const token = await AuthServices.login(username, password);
+    await storeToken(token, username);
     res.status(200).json({ token });
   } catch (error) {
     return res.status(400).json({ error: error.message });
@@ -27,7 +29,12 @@ const validateToken = async (req, res) => {
     }
     const token = authHeaders && authHeaders.split(' ')[1];
     const user = await AuthServices.validate(token);
-    res.status(200).json(user);
+    const username = await getToken(token);
+    if (!username) {
+      res.status(401).json({ error: 'Invalid token' });
+    } else {
+      res.status(200).json(user);
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
