cb01023cfb4306ba7d945c8f5315d026e7f580c8
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-08 15:55:42 +0530

    feat: add joi middleware validations for request body and headers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,21 @@
 const { Router } = require('express');
 const AuthController = require('../controllers/auth.controller');
+const { Schemas, validate } = require('../middlewares/auth.validator');
 const router = Router();
 
-router.post('/register', AuthController.registerUser);
-router.post('/login', AuthController.loginUser);
-router.get('/token/validate', AuthController.validateToken);
+router.post(
+  '/register',
+  validate(Schemas.registerSchema, 'body'),
+  AuthController.registerUser
+);
+router.post(
+  '/login',
+  validate(Schemas.loginSchema, 'body'),
+  AuthController.loginUser
+);
+router.get(
+  '/token/validate',
+  validate(Schemas.tokenHeaderSchema, 'headers'),
+  AuthController.validateToken
+);
 module.exports = router;
