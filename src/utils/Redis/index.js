d435d4a031f1573f9042022a976857c108d04c3f
Sukhman S <sukhmansindhipb05@gmail.com>
2023-07-11 17:40:34 +0530

    (feat): configured dotenv for redis utils

--- src/utils/Redis/index.js
+++ src/utils/Redis/index.js
@@ -1,8 +1,8 @@
 const { createClient } = require('redis');
 const config = {
   socket: {
-    host: 'redis',
-    port: 6379,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
   },
 };
 
